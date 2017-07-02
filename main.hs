eb343e29555a5d42fcf9a5a2153cb69876867851
NivenT <nachenjang@gmail.com>
2017-07-01 17:16:19 -0700

    added randomness

--- main.hs
+++ main.hs
@@ -1,5 +1,6 @@
 module Main (main) where
 
+import System.Random
 import Data.ByteString as BS
 
 import Graphics.Gloss
@@ -10,8 +11,12 @@ import Op
 
 main :: IO ()
 main = do
+  rng <- getStdGen
   game <- BS.readFile "games/PONG"
-  play window background fps (load_game init_emu (unpack game)) render_emu handle_events step_world
+
+  let emu = load_game init_emu (unpack game)
+  let world = (rng, emu)
+  play window background fps world draw_world handle_events step_world
 
 window :: Display
 window = InWindow "Chip-8" (800, 600) (10, 10)
@@ -22,11 +27,14 @@ background = black
 fps :: Int
 fps = 60
 
-handle_events :: Event -> Chip8 -> Chip8
+draw_world :: (RandomGen g) => World g -> Picture
+draw_world (_, emu) = render_emu emu
+
+handle_events :: (RandomGen g) => Event -> World g -> World g
 handle_events _ w = w
 
-step_world :: Float -> Chip8 -> Chip8
-step_world _ w = case exec_op w (get_opcode w) of
-  Left emu -> decr_timers . incr_pc $ emu
+step_world :: (RandomGen g) => Float -> World g -> World g
+step_world _ (rng, emu) = case exec_op emu (get_opcode emu) rng of
+  Left (rng, emu) -> (rng, decr_timers . incr_pc $ emu)
   Right err -> error err
     
--- op.hs
+++ op.hs
@@ -1,17 +1,33 @@
 module Op (
-  exec_op
+  exec_op,
+  World
 ) where 
 
+import System.Random
 import Data.Bits
+import Data.Word
 import Numeric
 
 import Emulator
 
-exec_op :: Chip8 -> Int -> Either Chip8 String
-exec_op emu op
+type World g = (g, Chip8)
+
+rand_byte :: (RandomGen g) => g -> (Word8, g)
+rand_byte = random
+
+rpl_nth :: [a] -> Int -> a -> [a]
+rpl_nth (x:xs) 0 y = y:xs
+rpl_nth (x:xs) n y = y:rpl_nth xs (n-1) y
+
+exec_op :: (RandomGen g) => Chip8 -> Int -> g -> Either (World g) String
+exec_op emu op rng
   | op `elem` [0xb000..0xbfff] = let n = (.&.) op 7
                                      p = pc emu
-                                 in Left emu{pc=p+n-2}
+                                 in Left (rng, emu{pc=p+n-2})
+  | op `elem` [0xc000..0xcfff] = let (r, g') = rand_byte rng
+                                     rs      = regs emu
+                                     val     = (.&.) r (rs!!0)
+                                 in Left (g', emu{regs=rpl_nth rs 0 val})
   | otherwise = Right $ "Instruction (0x" ++ (showHex op "") ++ ") has not yet been implemented"
     where x = (`shiftR` 2) $ (.&.) op 4
           y = (`shiftR` 1) $ (.&.) op 2
