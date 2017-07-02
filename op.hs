aca0a2100461882cf9e31c20cdbb76d00b991015
NivenT <nachenjang@gmail.com>
2017-07-01 20:46:02 -0700

    more instructions

--- op.hs
+++ op.hs
@@ -40,18 +40,27 @@ draw_sprite emu x y h = emu{screen=chunksOf 32 s', regs=rpl_nth rs 0xf (if flag 
                       bit = 0 /= ((.&.) (m!!(p+fromIntegral i)) (shiftR 128 j))
                   in (xor v bit, bit && (bit == v))
 
+-- Todo: Replace `elem` with in_range function
 exec_op :: (RandomGen g) => Chip8 -> Int -> g -> Either (World g) String
 exec_op emu op rng
-  | op `elem` [0xb000..0xbfff] = let n = (.&.) op 12
+  | op `elem` [0x6000..0x6fff] = let n = fromIntegral $ (.&.) op 0x00ff
+                                 in Left (rng, emu{regs=rpl_nth rs x n})
+  | op `elem` [0x7000..0x7fff] = let n = fromIntegral $ (.&.) op 0x00ff
+                                 in Left (rng, emu{regs=rpl_nth rs x (vx+n)})
+  | op `elem` [0xb000..0xbfff] = let n = (.&.) op 0x0fff
                                  in Left (rng, emu{pc=p+n-2})
   | op `elem` [0xc000..0xcfff] = let (r, g') = rand_byte rng
-                                     n       = fromIntegral $ (.&.) op 8
+                                     n       = fromIntegral $ (.&.) op 0x00ff
                                      val     = (.&.) r n
                                  in Left (g', emu{regs=rpl_nth rs x val})
   | op `elem` [0xd000..0xdfff] = Left (rng, draw_sprite emu x y $ (.&.) op 4)
-  | ((.&.) op 0xF000) == 0xe && ((.&.) op 0x00ff) == 0x9e = if ks!!(fromIntegral . toInteger $ vx)
+  | ((.&.) op 0xf000) == 0xe && ((.&.) op 0x00ff) == 0x9e = if ks!!(fromIntegral . toInteger $ vx)
                                                                then Left (rng, emu{pc=p+2})
                                                                else Left (rng, emu)
+  | ((.&.) op 0xf000) == 0xe && ((.&.) op 0x00ff) == 0xa1 = if ks!!(fromIntegral . toInteger $ vx)
+                                                               then Left (rng, emu)
+                                                               else Left (rng, emu{pc=p+2})
+  
   | otherwise = Right $ "Instruction (0x" ++ (showHex op "") ++ ") has not yet been implemented (X = " ++ (showHex x "") ++ " | Y = " ++ (showHex y "") ++ ")" 
     where x = (`shiftR` 8) $ (.&.) op 0x0F00
           y = (`shiftR` 4) $ (.&.) op 0x00F0
